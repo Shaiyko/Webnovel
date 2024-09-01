521747e6515abe68f32b599d6c6d8442c15a4780
Shaiyko <Dexprojectkk@gmail.com>
2024-09-01 23:55:15 +0700

    S123

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from "react";
-import { Backdrop, Box, CircularProgress } from "@mui/material";
+import { lazy } from "react";
+import {  Box } from "@mui/material";
+import PrivateRoute from "./cacklogin/ceaklogin";
+import Appbar from "./Appbar";
 
 const Home = lazy(() => import("./home"));
 const LoginRegister = lazy(() => import("./page/pagelogin/login"));
 const Adminhomepage = lazy(() => import("./page/pagemanagement/adminhomepage"));
 const Userhomepage = lazy(() => import("./page/pagemanagement/userpagehome"));
 const Category = lazy(() => import("./page/pagenovel/Category"));
-const Appbar = lazy(() => import("./Appbar"));
 const Profile = lazy(() => import("./page/pageprofile/profile"));
 const Bookshelf = lazy(() => import("./page/pagenovel/Bookshelf"));
 const TableAdmin = lazy(() => import("./page/pagemadmin/Admin"));
@@ -72,9 +73,7 @@ const ChangeData = lazy(() => import("./page/pageprofile/ChangeData"));
 const Applytobeanauthor = lazy(() =>
   import("./page/pageprofile/Applytobeanauthor")
 );
-const PrivateRoute = lazy(() => import("./cacklogin/ceaklogin"));
 const Ser = lazy(() => import("./ser"));
-
 function App() {
   return (
     <Box
@@ -93,152 +92,192 @@ function App() {
       }}
     >
       <Router>
-        <Suspense
-          fallback={
-            <div>
-              {" "}
-              <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                {/* ตัวโหลด */}
-                <Backdrop
-                  sx={{
-                    color: "#fff",
-                    zIndex: (theme) => theme.zIndex.drawer + 1,
-                  }}
-                >
-                  <CircularProgress color="inherit" />
-                </Backdrop>
-              </Box>
-            </div>
-          }
-        >
-          <Appbar />
-          <Box sx={{ width: "100%", marginTop: 2 }}>
-            <Routes>
-              
-              <Route path="/test" element={<ResponsiveSlider />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<LoginRegister />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/ms1" element={<Ser />} />
-              <Route
-                path="/applytobeanauthor"
-                element={<Applytobeanauthor />}
-              />
-              <Route path="/changedata" element={<ChangeData />} />
-              <Route path="/change-password" element={<ChangePassword />} />
-              <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Appbar />
+        <Box sx={{ width: "100%", marginTop: 2 }}>
+          <Routes>
+            <Route path="/test" element={<ResponsiveSlider />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginRegister />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/ms" element={<Ser />} />
+            <Route path="/applytobeanauthor" element={<Applytobeanauthor />} />
+            <Route path="/changedata" element={<ChangeData />} />
+            <Route path="/change-password" element={<ChangePassword />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            {
               <Route
                 path="/profile"
-                element={<PrivateRoute element={<Account />} />}
+                element={
+                   <PrivateRoute element={<Account />} path="/profile" />
+                }
               />
+            }
+            {/** */}
+            {
               <Route
-                path="/tnovel/:id_author"
-                element={<PrivateRoute element={<TableNovel />} />}
+                path="/manage/tnovel/:id_author"
+                element={
+                   <PrivateRoute element={<TableNovel />} path="/manage/tnovel/:id_author" />
+                }
               />
+            }
+             {
               <Route
-                path="/tnovel"
-                element={<PrivateRoute element={<TableNovelAdmin />} />}
+                path="/manage/tnovel"
+                element={
+                   <PrivateRoute element={<TableNovelAdmin />} path="/manage/tnovel" />
+                }
               />
-              <Route
-                path="/ttag"
-                element={<PrivateRoute element={<EnhancedTable />} />}
-              />
-              <Route
-                path="/ttype"
-                element={<PrivateRoute element={<TypeEnhancedTable />} />}
-              />
-              <Route
-                path="/tadmin"
-                element={<PrivateRoute element={<TableAdmin />} />}
-              />
-              <Route
-                path="/tuser"
-                element={<PrivateRoute element={<TableUse />} />}
-              />
-              <Route
-                path="/tsuggestions"
-                element={<PrivateRoute element={<TableSuggestions />} />}
-              />
-              <Route
-                path="/tauthor"
-                element={<PrivateRoute element={<TableAuthor />} />}
-              />
-              <Route path="/content/:id_novel" element={<ViewAndChapter />} />
-              <Route path="/content/updata/:id" element={<UpdateContent />} />
-              <Route
-                path="/contenttable/:id_novel"
-                element={<ContentNovel />}
-              />
-              <Route
-                path="/contentnew/:id_novel"
-                element={<CreateContentAdd />}
-              />
+            }
 
-              <Route path="/:id" element={<Category />} />
-              <Route
-                path="/admin"
-                element={<PrivateRoute element={<Adminhomepage />} />}
-              />
-              <Route
-                path="/user"
-                element={<PrivateRoute element={<Userhomepage />} />}
-              />
-              <Route
-                path="/profile"
-                element={<PrivateRoute element={<Profile />} />}
-              />
-              <Route
-                path="/my-bookshelf"
-                element={<PrivateRoute element={<Bookshelf />} />}
-              />
-              <Route
-                path="/suggestionreport/:id_novel"
-                element={<PrivateRoute element={<SuggestionReport />} />}
-              />
-              <Route
-                path="/suggestions"
-                element={<PrivateRoute element={<SuggestionForm2 />} />}
-              />
-              <Route path="/:searchN" element={<SearchNovel />} />
-              <Route path="/:searchTag" element={<SearchTag />} />
-              <Route path="/:searchType" element={<SearchType />} />
-              <Route path="/:searchA" element={<SearchAuthor />} />
-              <Route path="/novel/:id_novel" element={<ViewNovel />} />
-              <Route
-                path="/novel/:id_novel/directory"
-                element={<Directory />}
-              />
-              <Route path="/novel/:id_novel/:id" element={<ViewContent />} />
-              <Route
-                path="/novel"
-                element={<PrivateRoute element={<NovelReport />} />}
-              />
-              <Route
-                path="/author"
-                element={<PrivateRoute element={<ReportAuthor />} />}
-              />
-              <Route
-                path="/user"
-                element={<PrivateRoute element={<ReportUsers />} />}
-              />
-              <Route
-                path="/suggestions"
-                element={<PrivateRoute element={<ReportSuggestions />} />}
-              />
-              <Route
-                path="/reortview"
-                element={<PrivateRoute element={<ReportReading />} />}
-              />
-            </Routes>
-          </Box>
-          <Footer />
-        </Suspense>
+            <Route
+              path="/manage/ttag"
+              element={
+                <PrivateRoute element={<EnhancedTable />} path="/manage/ttag" />
+              }
+            />
+            <Route
+              path="/manage/ttype"
+              element={
+                <PrivateRoute
+                  element={<TypeEnhancedTable />}
+                  path="/manage/ttype"
+                />
+              }
+            />
+            <Route
+              path="/manage/tadmin"
+              element={
+                <PrivateRoute element={<TableAdmin />} path="/manage/tadmin" />
+              }
+            />
+            <Route
+              path="/manage/tuser"
+              element={
+                <PrivateRoute element={<TableUse />} path="/manage/tuser" />
+              }
+            />
+            <Route
+              path="/manage/suggestions"
+              element={
+                <PrivateRoute
+                  element={<TableSuggestions />}
+                  path="/manage/tuser"
+                />
+              }
+            />
+            <Route
+              path="/manage/tauthor"
+              element={
+                <PrivateRoute
+                  element={<TableAuthor />}
+                  path="//manage/tauthor"
+                />
+              }
+            />
+
+            <Route path="/content/:id_novel" element={<ViewAndChapter />} />
+            <Route path="/content/updata/:id" element={<UpdateContent />} />
+            <Route path="/contenttable/:id_novel" element={<ContentNovel />} />
+            <Route
+              path="/contentnew/:id_novel"
+              element={<CreateContentAdd />}
+            />
+
+            {/*  */}
+            <Route path="/novel-category/:id" element={<Category />} />
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute element={<Adminhomepage />} path="/admin" />
+              }
+            />
+            <Route
+              path="/user"
+              element={<PrivateRoute element={<Userhomepage />} path="/user" />}
+            />
+            <Route
+              path="/profile"
+              element={<PrivateRoute element={<Profile />} path="/profile" />}
+            />
+            <Route
+              path="/my-bookshelf"
+              element={
+                <PrivateRoute element={<Bookshelf />} path="/my-bookshelf" />
+              }
+            />
+
+            <Route
+              path="/suggestionreport/:id_novel"
+              element={
+                <PrivateRoute
+                  element={<SuggestionReport />}
+                  path="/suggestionreport/:id_novel"
+                />
+              }
+            />
+            <Route
+              path="/suggestions"
+              element={
+                <PrivateRoute
+                  element={<SuggestionForm2 />}
+                  path="/suggestions"
+                />
+              }
+            />
+            {/* Search */}
+            <Route path="/searchn/:searchN" element={<SearchNovel />} />
+            <Route path="/selecttag/:searchTag" element={<SearchTag />} />
+            <Route path="/selecttype/:searchType" element={<SearchType />} />
+            <Route path="/author/:searchA" element={<SearchAuthor />} />
+            {/* Search */}
+            <Route path="/novel/:id_novel" element={<ViewNovel />} />
+            <Route path="/novel/:id_novel/directory" element={<Directory />} />
+            <Route path="/novel/:id_novel/:id" element={<ViewContent />} />
+            {/* Report */}
+            <Route
+              path="/report/novel"
+              element={
+                <PrivateRoute element={<NovelReport />} path="/report/novel" />
+              }
+            />
+            <Route
+              path="/report/author"
+              element={
+                <PrivateRoute
+                  element={<ReportAuthor />}
+                  path="/report/author"
+                />
+              }
+            />
+            <Route
+              path="/report/user"
+              element={
+                <PrivateRoute element={<ReportUsers />} path="/report/user" />
+              }
+            />
+            <Route
+              path="/report/suggestions"
+              element={
+                <PrivateRoute
+                  element={<ReportSuggestions />}
+                  path="/report/suggestions"
+                />
+              }
+            />
+            <Route
+              path="/report/reortview"
+              element={
+                <PrivateRoute
+                  element={<ReportReading />}
+                  path="/report/reortview"
+                />
+              }
+            />
+          </Routes>
+        </Box>
       </Router>
+      <Footer />
     </Box>
   );
 }
