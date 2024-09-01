e863456b7f6930e39119770e37918381c4ef3f08
Shaiyko <Dexprojectkk@gmail.com>
2024-09-01 20:39:32 +0700

    up01

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,80 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./home";
-import LoginRegister from "./page/pagelogin/login";
-import Adminhomepage from "./page/pagemanagement/adminhomepage";
-import Userhomepage from "./page/pagemanagement/userpagehome";
-import Category from "./page/pagenovel/Category";
-import Appbar from "./Appbar";
-import Profile from "./page/pageprofile/profile";
-import Bookshelf from "./page/pagenovel/Bookshelf";
-import TableAdmin from "./page/pagemadmin/Admin";
-import TableUse from "./page/pagemanagement/User/User";
-import TableAuthor from "./page/pagemauthor/Author";
-import EnhancedTable from "./page/pagemanagement/Tag/TAgnovel";
-import TypeEnhancedTable from "./page/pagemanagement/Type/TypeNovel";
-import Register from "./page/pagelogin/Register";
-import { Box } from "@mui/material";
-import SearchNovel from "./Search/SearchNovel";
-import SearchAuthor from "./Search/SearchAuthor";
-import ViewNovel from "./page/pagenovel/Viewnovel";
-import TableNovel from "./page/pagemanagement/novel/Tablenovel";
-import ViewAndChapter from "./page/pagemanagement/novel/ViewNovelAdd";
-import ContentNovel from "./page/pagemanagement/novel/contentNovel/CreateContent";
-import CreateContentAdd from "./page/pagemanagement/novel/contentNovel/CreateContentAdd";
-import UpdateContent from "./page/pagemanagement/novel/contentNovel/UpdateConent";
-import ViewContent from "./page/pagenovel/ViewContent";
-import SearchTag from "./Search/SearchTag";
-import SearchType from "./Search/SearchType";
-import SuggestionReport from "./page/pagenovel/Suggestionreposrt";
-import SuggestionForm2 from "./page/pagenovel/SuggestionForm";
-import NovelReport from "./page/pagemanagement/Report/ReportNovel";
-import ReportAuthor from "./page/pagemanagement/Report/ReportAuthor";
-import ReportUsers from "./page/pagemanagement/Report/ReportUsers";
-import ReportSuggestions from "./page/pagemanagement/Report/ReportSuggestions";
-import ReportReading from "./page/pagemanagement/Report/ReportReading";
-import TableSuggestions from "./page/pagemanagement/Report/Suggestions/TableSuggestions";
-import Directory from "./page/pagenovel/Directory";
-import Footer from "./Footerweb";
-import ResponsiveSlider from "./Test/test";
-import TableNovelAdmin from "./page/pagemanagement/TablenovelAddmin";
-import Account from "./page/pageprofile/account";
-import ForgotPassword from "./page/pagelogin/ForgotPassword";
-import ChangePassword from "./page/pageprofile/ChangePassword";
-import ChangeData from "./page/pageprofile/ChangeData";
-import Applytobeanauthor from "./page/pageprofile/Applytobeanauthor";
-import PrivateRoute from "./cacklogin/ceaklogin";
-import Ser from "./ser";
+import { Suspense, lazy } from "react";
+import { Backdrop, Box, CircularProgress } from "@mui/material";
+
+const Home = lazy(() => import("./home"));
+const LoginRegister = lazy(() => import("./page/pagelogin/login"));
+const Adminhomepage = lazy(() => import("./page/pagemanagement/adminhomepage"));
+const Userhomepage = lazy(() => import("./page/pagemanagement/userpagehome"));
+const Category = lazy(() => import("./page/pagenovel/Category"));
+const Appbar = lazy(() => import("./Appbar"));
+const Profile = lazy(() => import("./page/pageprofile/profile"));
+const Bookshelf = lazy(() => import("./page/pagenovel/Bookshelf"));
+const TableAdmin = lazy(() => import("./page/pagemadmin/Admin"));
+const TableUse = lazy(() => import("./page/pagemanagement/User/User"));
+const TableAuthor = lazy(() => import("./page/pagemauthor/Author"));
+const EnhancedTable = lazy(() => import("./page/pagemanagement/Tag/TAgnovel"));
+const TypeEnhancedTable = lazy(() =>
+  import("./page/pagemanagement/Type/TypeNovel")
+);
+const Register = lazy(() => import("./page/pagelogin/Register"));
+const SearchNovel = lazy(() => import("./Search/SearchNovel"));
+const SearchAuthor = lazy(() => import("./Search/SearchAuthor"));
+const ViewNovel = lazy(() => import("./page/pagenovel/Viewnovel"));
+const TableNovel = lazy(() => import("./page/pagemanagement/novel/Tablenovel"));
+const ViewAndChapter = lazy(() =>
+  import("./page/pagemanagement/novel/ViewNovelAdd")
+);
+const ContentNovel = lazy(() =>
+  import("./page/pagemanagement/novel/contentNovel/CreateContent")
+);
+const CreateContentAdd = lazy(() =>
+  import("./page/pagemanagement/novel/contentNovel/CreateContentAdd")
+);
+const UpdateContent = lazy(() =>
+  import("./page/pagemanagement/novel/contentNovel/UpdateConent")
+);
+const ViewContent = lazy(() => import("./page/pagenovel/ViewContent"));
+const SearchTag = lazy(() => import("./Search/SearchTag"));
+const SearchType = lazy(() => import("./Search/SearchType"));
+const SuggestionReport = lazy(() =>
+  import("./page/pagenovel/Suggestionreposrt")
+);
+const SuggestionForm2 = lazy(() => import("./page/pagenovel/SuggestionForm"));
+const NovelReport = lazy(() =>
+  import("./page/pagemanagement/Report/ReportNovel")
+);
+const ReportAuthor = lazy(() =>
+  import("./page/pagemanagement/Report/ReportAuthor")
+);
+const ReportUsers = lazy(() =>
+  import("./page/pagemanagement/Report/ReportUsers")
+);
+const ReportSuggestions = lazy(() =>
+  import("./page/pagemanagement/Report/ReportSuggestions")
+);
+const ReportReading = lazy(() =>
+  import("./page/pagemanagement/Report/ReportReading")
+);
+const TableSuggestions = lazy(() =>
+  import("./page/pagemanagement/Report/Suggestions/TableSuggestions")
+);
+const Directory = lazy(() => import("./page/pagenovel/Directory"));
+const Footer = lazy(() => import("./Footerweb"));
+const ResponsiveSlider = lazy(() => import("./Test/test"));
+const TableNovelAdmin = lazy(() =>
+  import("./page/pagemanagement/TablenovelAddmin")
+);
+const Account = lazy(() => import("./page/pageprofile/account"));
+const ForgotPassword = lazy(() => import("./page/pagelogin/ForgotPassword"));
+const ChangePassword = lazy(() => import("./page/pageprofile/ChangePassword"));
+const ChangeData = lazy(() => import("./page/pageprofile/ChangeData"));
+const Applytobeanauthor = lazy(() =>
+  import("./page/pageprofile/Applytobeanauthor")
+);
+const PrivateRoute = lazy(() => import("./cacklogin/ceaklogin"));
+const Ser = lazy(() => import("./ser"));
+
 function App() {
   return (
     <Box
@@ -62,198 +93,150 @@ function App() {
       }}
     >
       <Router>
-        <Appbar />
-        <Box sx={{ width: "100%", marginTop: 2 }}>
-          <Routes>
-            <Route path="/test" element={<ResponsiveSlider />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<LoginRegister />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/ms/t" element={<Ser />} />
-            <Route path="/ms/t/1" element={<Ser />} />
-            <Route path="/ms/t/1/u" element={<Ser />} />
-            <Route path="/applytobeanauthor" element={<Applytobeanauthor />} />
-            <Route path="/changedata" element={<ChangeData />} />
-            <Route path="/change-password" element={<ChangePassword />} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-            {
+        <Suspense
+          fallback={
+            <div>
+              {" "}
+              <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                {/* ตัวโหลด */}
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                  }}
+                >
+                  <CircularProgress color="inherit" />
+                </Backdrop>
+              </Box>
+            </div>
+          }
+        >
+          <Appbar />
+          <Box sx={{ width: "100%", marginTop: 2 }}>
+            <Routes>
+              <Route path="/test" element={<ResponsiveSlider />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<LoginRegister />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/ms/t" element={<Ser />} />
+              <Route
+                path="/applytobeanauthor"
+                element={<Applytobeanauthor />}
+              />
+              <Route path="/changedata" element={<ChangeData />} />
+              <Route path="/change-password" element={<ChangePassword />} />
+              <Route path="/forgotpassword" element={<ForgotPassword />} />
               <Route
                 path="/profile"
-                element={<PrivateRoute element={<Account />} path="/profile" />}
+                element={<PrivateRoute element={<Account />} />}
               />
-            }
-            {/** */}
-            {
               <Route
                 path="/manage/tnovel/:id_author"
-                element={
-                  <PrivateRoute
-                    element={<TableNovel />}
-                    path="/manage/tnovel/:id_author"
-                  />
-                }
+                element={<PrivateRoute element={<TableNovel />} />}
               />
-            }
-            {
               <Route
                 path="/manage/tnovel"
-                element={
-                  <PrivateRoute
-                    element={<TableNovelAdmin />}
-                    path="/manage/tnovel"
-                  />
-                }
+                element={<PrivateRoute element={<TableNovelAdmin />} />}
               />
-            }
-
-            <Route
-              path="/manage/ttag"
-              element={
-                <PrivateRoute element={<EnhancedTable />} path="/manage/ttag" />
-              }
-            />
-            <Route
-              path="/manage/ttype"
-              element={
-                <PrivateRoute
-                  element={<TypeEnhancedTable />}
-                  path="/manage/ttype"
-                />
-              }
-            />
-            <Route
-              path="/manage/tadmin"
-              element={
-                <PrivateRoute element={<TableAdmin />} path="/manage/tadmin" />
-              }
-            />
-            <Route
-              path="/manage/tuser"
-              element={
-                <PrivateRoute element={<TableUse />} path="/manage/tuser" />
-              }
-            />
-            <Route
-              path="/manage/suggestions"
-              element={
-                <PrivateRoute
-                  element={<TableSuggestions />}
-                  path="/manage/tuser"
-                />
-              }
-            />
-            <Route
-              path="/manage/tauthor"
-              element={
-                <PrivateRoute
-                  element={<TableAuthor />}
-                  path="//manage/tauthor"
-                />
-              }
-            />
-
-            <Route path="/content/:id_novel" element={<ViewAndChapter />} />
-            <Route path="/content/updata/:id" element={<UpdateContent />} />
-            <Route path="/contenttable/:id_novel" element={<ContentNovel />} />
-            <Route
-              path="/contentnew/:id_novel"
-              element={<CreateContentAdd />}
-            />
-
-            {/*  */}
-            <Route path="/novel-category/:id" element={<Category />} />
-            <Route
-              path="/admin"
-              element={
-                <PrivateRoute element={<Adminhomepage />} path="/admin" />
-              }
-            />
-            <Route
-              path="/user"
-              element={<PrivateRoute element={<Userhomepage />} path="/user" />}
-            />
-            <Route
-              path="/profile"
-              element={<PrivateRoute element={<Profile />} path="/profile" />}
-            />
-            <Route
-              path="/my-bookshelf"
-              element={
-                <PrivateRoute element={<Bookshelf />} path="/my-bookshelf" />
-              }
-            />
-
-            <Route
-              path="/suggestionreport/:id_novel"
-              element={
-                <PrivateRoute
-                  element={<SuggestionReport />}
-                  path="/suggestionreport/:id_novel"
-                />
-              }
-            />
-            <Route
-              path="/suggestions"
-              element={
-                <PrivateRoute
-                  element={<SuggestionForm2 />}
-                  path="/suggestions"
-                />
-              }
-            />
-            {/* Search */}
-            <Route path="/searchn/:searchN" element={<SearchNovel />} />
-            <Route path="/selecttag/:searchTag" element={<SearchTag />} />
-            <Route path="/selecttype/:searchType" element={<SearchType />} />
-            <Route path="/author/:searchA" element={<SearchAuthor />} />
-            {/* Search */}
-            <Route path="/novel/:id_novel" element={<ViewNovel />} />
-            <Route path="/novel/:id_novel/directory" element={<Directory />} />
-            <Route path="/novel/:id_novel/:id" element={<ViewContent />} />
-            {/* Report */}
-            <Route
-              path="/report/novel"
-              element={
-                <PrivateRoute element={<NovelReport />} path="/report/novel" />
-              }
-            />
-            <Route
-              path="/report/author"
-              element={
-                <PrivateRoute
-                  element={<ReportAuthor />}
-                  path="/report/author"
-                />
-              }
-            />
-            <Route
-              path="/report/user"
-              element={
-                <PrivateRoute element={<ReportUsers />} path="/report/user" />
-              }
-            />
-            <Route
-              path="/report/suggestions"
-              element={
-                <PrivateRoute
-                  element={<ReportSuggestions />}
-                  path="/report/suggestions"
-                />
-              }
-            />
-            <Route
-              path="/report/reortview"
-              element={
-                <PrivateRoute
-                  element={<ReportReading />}
-                  path="/report/reortview"
-                />
-              }
-            />
-          </Routes>
-        </Box>
+              <Route
+                path="/manage/ttag"
+                element={<PrivateRoute element={<EnhancedTable />} />}
+              />
+              <Route
+                path="/manage/ttype"
+                element={<PrivateRoute element={<TypeEnhancedTable />} />}
+              />
+              <Route
+                path="/manage/tadmin"
+                element={<PrivateRoute element={<TableAdmin />} />}
+              />
+              <Route
+                path="/manage/tuser"
+                element={<PrivateRoute element={<TableUse />} />}
+              />
+              <Route
+                path="/manage/suggestions"
+                element={<PrivateRoute element={<TableSuggestions />} />}
+              />
+              <Route
+                path="/manage/tauthor"
+                element={<PrivateRoute element={<TableAuthor />} />}
+              />
+              <Route path="/content/:id_novel" element={<ViewAndChapter />} />
+              <Route path="/content/updata/:id" element={<UpdateContent />} />
+              <Route
+                path="/contenttable/:id_novel"
+                element={<ContentNovel />}
+              />
+              <Route
+                path="/contentnew/:id_novel"
+                element={<CreateContentAdd />}
+              />
+              <Route path="/novel-category/:id" element={<Category />} />
+              <Route
+                path="/admin"
+                element={<PrivateRoute element={<Adminhomepage />} />}
+              />
+              <Route
+                path="/user"
+                element={<PrivateRoute element={<Userhomepage />} />}
+              />
+              <Route
+                path="/profile"
+                element={<PrivateRoute element={<Profile />} />}
+              />
+              <Route
+                path="/my-bookshelf"
+                element={<PrivateRoute element={<Bookshelf />} />}
+              />
+              <Route
+                path="/suggestionreport/:id_novel"
+                element={<PrivateRoute element={<SuggestionReport />} />}
+              />
+              <Route
+                path="/suggestions"
+                element={<PrivateRoute element={<SuggestionForm2 />} />}
+              />
+              <Route path="/searchn/:searchN" element={<SearchNovel />} />
+              <Route path="/selecttag/:searchTag" element={<SearchTag />} />
+              <Route path="/selecttype/:searchType" element={<SearchType />} />
+              <Route path="/author/:searchA" element={<SearchAuthor />} />
+              <Route path="/novel/:id_novel" element={<ViewNovel />} />
+              <Route
+                path="/novel/:id_novel/directory"
+                element={<Directory />}
+              />
+              <Route path="/novel/:id_novel/:id" element={<ViewContent />} />
+              <Route
+                path="/report/novel"
+                element={<PrivateRoute element={<NovelReport />} />}
+              />
+              <Route
+                path="/report/author"
+                element={<PrivateRoute element={<ReportAuthor />} />}
+              />
+              <Route
+                path="/report/user"
+                element={<PrivateRoute element={<ReportUsers />} />}
+              />
+              <Route
+                path="/report/suggestions"
+                element={<PrivateRoute element={<ReportSuggestions />} />}
+              />
+              <Route
+                path="/report/reortview"
+                element={<PrivateRoute element={<ReportReading />} />}
+              />
+            </Routes>
+          </Box>
+          <Footer />
+        </Suspense>
       </Router>
-      <Footer />
     </Box>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // แยก chunk สำหรับไลบรารีหลัก ๆ เช่น react, mui เป็นต้น
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
+    // เพิ่มขีดจำกัดคำเตือน chunk size เป็น 2MB (ค่าดีฟอลต์คือ 500KB)
+    chunkSizeWarningLimit: 2000,
+  },
 });
